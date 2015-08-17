e8a2436cb5a5869ab93b1d9e02e6d599d1d2e48c
GathoMauricio <mauricio2769@gmail.com>
2015-08-17 03:29:31 -0500

    geolocalizacion lista

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -1,51 +1,103 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
-    // Application Constructor
+    // Constructor
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+    // Agregando escuchadores
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+    // Se dispara cuando el dispositivo ha terminado de cargar
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        //habilitando 2do plano
+                window.plugin.backgroundMode.enable();
+         cordova.plugins.backgroundMode.setDefaults(
+                                                        {
+                                                        title:'DotRedes-DesafiandoRetos.',
+                                                        text:'Esta App funciona en segundo plano.'
+                                                        });
     },
-    // Update DOM on a Received Event
+    // Recibe el evento disparado y lo muestra en consola
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+       console.log('Evento recibido: ' + id);
     }
 };
-
+//inicializa la aplicación
 app.initialize();
+$(document).ready(function(e) {
+  $("div:jqmData(role='panel')").css('margin-top',  ($("div:jqmData(role='header')").height()));
+});
+
+function iniciarSesion(){
+var usuario=$("#txt_usuario_login").prop("value");
+var contrasena=$("#txt_contrasena_login").prop("value");
+$.post("http://dotredes.dyndns.biz:18888/dot_izzi/mobile/iniciar_sesion.php",{usuario:usuario,contrasena:contrasena},function(data){
+    //validando
+    if(data >= 1){
+        //seteando variable de sesion
+        window.localStorage.setItem("id_empleado",data);
+
+        window.location ="inicio.html";
+    }else{
+        alert("Los datos introducidos son incorrectos");
+    }
+});
+}
+
+function cerrarSesion()
+{
+
+    swal({
+            title: "atencion",
+            text: "Al cerrar sesion se detendra el envio de ubicacion y otras funciones",
+            showCancelButton: true,
+            confirmButtonColor: "#086A87",
+            type:'warning',
+            confirmButtonText: "Cerrar sesion",
+            closeOnConfirm: false },
+            function(){
+                //detener segundo plano e hilo decoordenadas
+                window.plugin.backgroundMode.disable();
+                window.localStorage.clear();
+                window.location ="index.html";
+
+            });
+
+}
+
+function buscarActualizacion(version)
+{
+    $.post("http://dotredes.dyndns.biz:18888/dot_izzi/mobile/get_version.php",{},function(data){
+        if(data > version){
+        swal({
+        title: "Nueva version encontrada",
+        text: "Descargar nueva version?",
+        showCancelButton: true,
+        confirmButtonColor: "#086A87",
+        confirmButtonText: "Descargar",
+        closeOnConfirm: false },
+        function(){
+        swal("","La descarga comenzara en un momento", "success");
+        navigator.app.loadUrl("http://dotredes.dyndns.biz:18888/dot_izzi/mobile/DotRedes.apk", { openExternal:true });
+        });
+        }else{
+            swal("","No hay actualizaciones disponibles","error");
+        }
+    });
+}
+
+function getLocation(location){
+$(".received").html("Lat: "+location.coords.latitude+"<br>Lon"+location.coords.longitude);
+$.post("http://dotredes.dyndns.biz:18888/dot_izzi/mobile/update_geolocalizacion.php",
+{
+id_empleado:window.localStorage.getItem("id_empleado"),
+lat:location.coords.latitude,
+lon:location.coords.longitude
+},function(data){
+console.log(data);
+});
+}
+function onError(error){
+$(".received").html("Ha ocurrido un error");
+}
